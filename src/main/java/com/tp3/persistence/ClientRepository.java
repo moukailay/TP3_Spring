7c375cae7f2f14de84dac67b3d66ad29c24d8aed
System Administrator <root@MacMikhail.local>
2022-04-11 05:11:04 -0400

    Refactor class ClientRepository

---
+++ src/main/java/com/tp3/persistence/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.tp3.persistence;
+
+import com.tp3.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Long countByIdUser ( Long id );
+}
