f39b63249ce9bf3472ffce0b5a1cd67cb5e8b5ff
System Administrator <root@MacMikhail.local>
2022-04-11 05:13:52 -0400

    Refactor class DocumentRepository

---
+++ src/main/java/com/tp3/persistence/DocumentRepository.java
@@ -0,0 +1,21 @@
+package com.tp3.persistence;
+
+import com.tp3.model.Client;
+import com.tp3.model.Document;
+import com.tp3.model.Livre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+    List<Document> findByGenre(String genre);
+    List<Document> findByAuteur(String auteur);
+    List<Document> findByTitreContains(String titre);
+
+    @Query("select c from Document c where YEAR(c.datePub) =:anneePub")
+    List<Document> findByAnnePub(@Param(("anneePub"))int text);
+}
