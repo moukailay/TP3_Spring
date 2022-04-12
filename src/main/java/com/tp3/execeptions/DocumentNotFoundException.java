f8147ba2cd3da52d943c285d2f31ec811906ebda
System Administrator <root@MacMikhail.local>
2022-04-12 07:16:20 -0400

    ajout class DocumentNotFoundExeption

---
+++ src/main/java/com/tp3/execeptions/DocumentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.tp3.execeptions;
+
+public class DocumentNotFoundException extends Throwable{
+    public DocumentNotFoundException ( String message ) {
+        super( message );
+    }
+}
